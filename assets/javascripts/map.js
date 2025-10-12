a876cb296b39d59bd14deba294643f6b02fc9b3d
Martin Coulthurst <mc@lightness.co.uk>
2025-10-12 15:08:02 +0100

    Instantiate all components once the page has loaded

--- assets/javascripts/map.js
+++ assets/javascripts/map.js
@@ -1,37 +1,57 @@
-console.log("here now", startPosn);
+console.log("here now");
 
-let center = [227050, 61750];
-if (startPosn) {
-  center = startPosn;
+
+
+function createMap(target, coords) {
+  console.log('create map ' + target, coords);
+
+  let center = [227050, 61750];
+
+  if (coords.length < 0) {
+    coords = center;
+  }
+
+  // Define the British National Grid projection
+  proj4.defs("EPSG:27700", "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs");
+  ol.proj.proj4.register(proj4);
+
+  // Create map with OSM tiles, passing in the target element and the coords
+  let baseLayer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
+
+  const map = new ol.Map({
+    target: target,
+    layers: [
+      baseLayer
+    ],
+    view: new ol.View({
+      projection: 'EPSG:27700',
+      center: coords,
+      zoom: 15
+    })
+  });
+
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.animate({
+    zoom: 16,
+    duration: 100
+  });
+
 }
-// Define the British National Grid projection
-proj4.defs("EPSG:27700", "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs");
-ol.proj.proj4.register(proj4);
 
-// Create map with OSM tiles
-let baseLayer = new ol.layer.Tile({
-  source: new ol.source.OSM()
-});
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log('ready');
+  //get all map class tags
+  let maps = document.getElementsByClassName('map');
 
-
-const map = new ol.Map({
-  target: 'map',
-  layers: [
-    baseLayer
-  ],
-  view: new ol.View({
-    projection: 'EPSG:27700',
-    center: center,
-    zoom: 15
-  })
-});
+  for (let i = 0; i < maps.length; i++) {
+    let target = 'map' + (i + 1);
+    let coords = JSON.parse(maps[i].dataset.coords);
+    maps[i].setAttribute('id', target);
+    createMap(target, coords);
+  }
 
 
-const view = map.getView();
-const zoom = view.getZoom();
-
-view.animate({
-  zoom: 16,
-  duration: 100
-});
-
+})
