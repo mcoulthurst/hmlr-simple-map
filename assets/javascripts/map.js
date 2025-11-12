6eb88e25962816559d65f5f53d8243ca338c7c12
Martin Coulthurst <mc@lightness.co.uk>
2025-11-12 15:08:23 +0000

    Tidy up

--- assets/javascripts/map.js
+++ assets/javascripts/map.js
@@ -9,7 +9,6 @@ function createMap(target, options) {
   let i = 0;
 
   console.log('create map ' + target, coords, zoom);
-  console.log(layerSettings);
 
   // define the British National Grid projection
   proj4.defs("EPSG:27700", "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs");
@@ -47,7 +46,6 @@ function createMap(target, options) {
     for( i=0; i<layerSettings.length; i++)
     {
       const style = getStyle(layerSettings[i].style);
-      console.log(style);
       const vectorSource = new ol.source.Vector();
       
       // create vector layer
@@ -333,7 +331,6 @@ var hatch_pattern = function (colour) {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('component loaded');
   // get all map class tags
   let maps = document.getElementsByClassName('hmlr-map');
   let options = {};
